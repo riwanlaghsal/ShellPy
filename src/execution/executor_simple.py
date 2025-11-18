421d0b239873074cbc6ca735a2900684ea3e6b34
ilyes <20234622@etud.univ-evry.fr>
2025-11-18 16:16:58 +0100

    add executor cmd simple (not final version)

--- src/execution/executor_simple.py
+++ src/execution/executor_simple.py
@@ -9,11 +9,25 @@ built_in = ["cd", "if", "for"]
 
 def exec_simple(cmd_simple):
     space = " "
-    cmd_simple[args] = space.join(cmd_simple[args])
-    if cmd_simple[cmd] in built_in:
-        if(cmd_simple[stdin])
-        subprocess.call(cmd_simple[cmd], cmd_simple[args[0]])
-    else:
+    cmd_simple["args"] = space.join(cmd_simple["args"])
+    cmd_built_in = None
 
-        subprocess.Popen(cmd_simple[cmd],cmd_simple[args[0]])
+    if cmd_simple["cmd"] in built_in:
+        if (cmd_simple["cmd"] == "cd"):
+            cmd_built_in = ["python3", "src/cmd_built_in/cd.py"] + cmd_simple["args"]
+        if (cmd_simple["cmd"] == "if"):
+            cmd_built_in = ["python3", "src/cmd_built_in/if.py"] + cmd_simple["args"]
+
+        if(cmd_simple["stdout"]):
+            with open(cmd_simple["stdout"]["filename"], cmd_simple["stdout"]["mode"]) as f:
+                proc = subprocess.Popen(cmd_built_in, stdout= f)
+                proc.wait()
+        elif (cmd_simple["stdin"]):
+            with open(cmd_simple["stdin"]["filename"]) as f:
+                proc = subprocess.Popen(cmd_built_in, stdin=f)
+                proc.wait()
+
+    else:
+        if (cmd_simple["stdout"]):
+            subprocess.Popen(cmd_simple["cmd"],cmd_simple["args"], stdout=cmd_simple["stdout"]["filename"])
 
