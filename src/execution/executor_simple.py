d26102a621395ce5f3feaffc244d04b4193de1b6
ilyesMeh <20234622@etud-univ.evry.fr>
2025-11-19 00:10:01 +0100

    exec_simple v1

--- src/execution/executor_simple.py
+++ src/execution/executor_simple.py
@@ -1,33 +1,38 @@
-import cmd
-
-from src.cmd_built_in import cd
-import src.parsing
+from ShellPy.src.cmd_built_in import cd
 import subprocess
-
-built_in = ["cd", "if", "for"]
+import redir
+from ShellPy.src.execution.redir import handle_redir
 
 
 def exec_simple(cmd_simple):
-    space = " "
-    cmd_simple["args"] = space.join(cmd_simple["args"])
+    built_in = ["cd", "if", "for"]
     cmd_built_in = None
+
+    stdin, stdout = handle_redir(cmd_simple)
+    if stdin == -1:
+        return -1
+    commande = [cmd_simple["cmd"]] + cmd_simple["args"]
 
     if cmd_simple["cmd"] in built_in:
         if (cmd_simple["cmd"] == "cd"):
-            cmd_built_in = ["python3", "src/cmd_built_in/cd.py"] + cmd_simple["args"]
+            cmd_built_in = ["python3", "ShellPy/src/cmd_built_in/cd.py"] + cmd_simple["args"]
         if (cmd_simple["cmd"] == "if"):
-            cmd_built_in = ["python3", "src/cmd_built_in/if.py"] + cmd_simple["args"]
-
-        if(cmd_simple["stdout"]):
-            with open(cmd_simple["stdout"]["filename"], cmd_simple["stdout"]["mode"]) as f:
-                proc = subprocess.Popen(cmd_built_in, stdout= f)
-                proc.wait()
-        elif (cmd_simple["stdin"]):
-            with open(cmd_simple["stdin"]["filename"]) as f:
-                proc = subprocess.Popen(cmd_built_in, stdin=f)
-                proc.wait()
+            cmd_built_in = ["python3", "ShellPy/src/cmd_built_in/if.py"] + cmd_simple["args"]
+        proc = subprocess.Popen(cmd_built_in, stdin=stdin, stdout=stdout)
+        proc.wait()
 
     else:
-        if (cmd_simple["stdout"]):
-            subprocess.Popen(cmd_simple["cmd"],cmd_simple["args"], stdout=cmd_simple["stdout"]["filename"])
+        proc = subprocess.Popen(commande, stdin=stdin, stdout=stdout)
+        proc.wait()
 
+simple_cmd = {
+    "type": "command",
+    "cmd": "cd",
+    "args": ["/src/parsing"],
+    "stdin": None,
+    "stdout": None,
+    "background": False
+}
+import os
+exec_simple(simple_cmd)
+print(os.getcwd())
