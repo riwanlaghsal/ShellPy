2e7bc155533542eef1a112d29cc0191c94779201
lazki <158278932+riwanlaghsal@users.noreply.github.com>
2025-11-18 20:42:23 +0100

    add gestion des redirections dans l'execution

---
+++ src/execution/redir.py
@@ -0,0 +1,19 @@
+import sys
+
+def handle_redir(cmd):
+    stdin = None
+    stdout = None
+
+    if cmd["stdout"]:
+        stdout = open(cmd["stdout"]["filename"], cmd["stdout"]["mode"])
+    if cmd["stdin"]:
+        try:
+            stdin = open(cmd["stdin"]["filename"], "r")
+        except FileNotFoundError:
+            print(f"Erreur : fichier '{cmd["stdin"]['file']}' introuvable", file=sys.stderr)
+            stdin = -1
+        except PermissionError:
+            print(f"Erreur : permission refusée pour le fichier '{cmd['stdin']['file']}'", file=sys.stderr)
+            stdin = -1
+
+    return stdin, stdout
