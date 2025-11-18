ae25e9716ea6b7bd90ecc56842d1b05ac6b4589d
ilyesMeh <20234622@etud-univ.evry.fr>
2025-11-19 00:53:34 +0100

    ajout + correction cd fonctionnel

--- src/execution/executor_simple.py
+++ src/execution/executor_simple.py
@@ -1,17 +1,17 @@
-from ShellPy.src.cmd_built_in import cd
+from ShellPy.src.cmd_built_in.cd import cd
 import subprocess
 import redir
 from ShellPy.src.execution.redir import handle_redir
 import os
 
+def get_path(cmd_simple):
+    path = str(cmd_simple["args"])
+    path = path[2:-2]
+    return path
 
 def exec_simple(cmd_simple):
     built_in = ["cd", "if", "for"]
     cmd_built_in = None
-
-    CURRENT_DIR = os.path.dirname(__file__)
-    CD_SCRIPT = os.path.join(CURRENT_DIR, "..", "cmd_built_in", "cd.py")
-    CD_SCRIPT = os.path.abspath(CD_SCRIPT)
 
     stdin, stdout = handle_redir(cmd_simple)
     if stdin == -1:
@@ -20,11 +20,10 @@ def exec_simple(cmd_simple):
 
     if cmd_simple["cmd"] in built_in:
         if (cmd_simple["cmd"] == "cd"):
-            cmd_built_in = ["python3", CD_SCRIPT] + cmd_simple["args"]
+            path = get_path(cmd_simple)
+            cd(path)
         if (cmd_simple["cmd"] == "if"):
             cmd_built_in = ["python3", "../cmd_built_in/if.py"] + cmd_simple["args"]
-        proc = subprocess.Popen(cmd_built_in, stdin=stdin, stdout=stdout)
-        proc.wait()
 
     else:
         proc = subprocess.Popen(commande, stdin=stdin, stdout=stdout)
@@ -33,7 +32,7 @@ def exec_simple(cmd_simple):
 simple_cmd = {
     "type": "command",
     "cmd": "cd",
-    "args": ["/src/parsing"],
+    "args": ["    .."],
     "stdin": None,
     "stdout": None,
     "background": False
