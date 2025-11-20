7073457d435a37c9ce115e74af8095ff3c5ea3ec
laghsal riwan <20233810@etud.univ-evry.fr>
2025-11-20 14:30:05 +0100

    ajout de l'exectuion des pipelines

---
+++ src/execution/exec_pipeline.py
@@ -0,0 +1,37 @@
+import subprocess
+import sys
+
+from redir import handle_redir
+from executor_simple import exec_simple
+
+def exec_pipe(pipe):
+    processus = []
+    prev_stdout = None
+    stdout_pipe = None
+    built_in = ["cd", "if", "for"]
+
+    for i, cmd in enumerate(pipe["commands"]):
+        stdin, stdout = handle_redir(cmd)
+        if prev_stdout:
+            stdin = prev_stdout
+
+        if i < len(pipe["commands"] - 1):
+            stdout_pipe = subprocess.PIPE
+        else:
+            stdout_pipe = stdout
+
+        if cmd["cmd"] in built_in:
+            exec_simple(cmd)
+        else:
+            commande = [cmd["cmd"]] + cmd["args"]
+            try:
+                proc = subprocess.Popen(commande, stdin=stdin, stdout=stdout_pipe)
+                processus.append(proc)
+                prev_stdout = proc.stdout
+            except FileNotFoundError:
+                print(f"Commande introuvable : {cmd['cmd']}", file=sys.stderr)
+
+    for proc in processus:
+        proc.wait()
+
+
