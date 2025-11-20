bcaeebbb4cd6f3315495ac71ddab16b8083c5aa0
riwanlaghsal <20233810@etud.univ-evry.fr>
2025-11-20 15:36:16 +0100

    ajout de l'expansion de variable dans le parsing

--- src/parsing/tokenizer.py
+++ src/parsing/tokenizer.py
@@ -1,9 +1,11 @@
 import shlex
 import os
+from expand_var import expand_var
 
 def tokenizer(line):
     tokens = shlex.split(line)
+    tokens = expand_var(tokens)
     return tokens
 
-
+# line = "echo $LANG"
 # print(tokenizer(line))
