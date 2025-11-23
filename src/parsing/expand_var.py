200b0b99b4b949dc63d0e5c99c5058130fd9540a
laghsal riwan <20233810@etud.univ-evry.fr>
2025-11-23 16:14:20 +0100

    retour a l'ancienne gestion des variables d'environnement

--- src/parsing/expand_var.py
+++ src/parsing/expand_var.py
@@ -4,10 +4,9 @@ def expand_var(tokens):
     new_tokens = []
     for token in tokens:
         if token.startswith("$"):
-            if token[0] != "'" and token[len(token) - 1 != "'"]:
-                var_name = token[1:]
-                value = os.environ.get(var_name, "")
-                new_tokens.append(value) # pas de gestion d'erreur comme un vrai shell (ex: echo n'affiche rien)
+            var_name = token[1:]
+            value = os.environ.get(var_name, "")
+            new_tokens.append(value) # pas de gestion d'erreur comme un vrai shell (ex: echo n'affiche rien)
         else:
             new_tokens.append(token)
     return new_tokens
