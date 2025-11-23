e3d4a49561d91c1c2a85bb26fcd4a619b02f8197
laghsal riwan <20233810@etud.univ-evry.fr>
2025-11-23 01:04:10 +0100

    ajout de la gestion des espaces entre redir et pipes

--- src/parsing/tokenizer.py
+++ src/parsing/tokenizer.py
@@ -3,9 +3,13 @@ import os
 from src.parsing.expand_var import expand_var
 
 def tokenizer(line):
+    line = line.replace(">", " > ")
+    line = line.replace(">>", " >> ")
+    line = line.replace("<", " < ")
+    line = line.replace("|", " | ")
     tokens = shlex.split(line)
     tokens = expand_var(tokens)
     return tokens
 
-# line = "echo $LANG"
+# line = "echo bjr>test.txt|ls"
 # print(tokenizer(line))
