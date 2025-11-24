0d214e5f49b66523058f62c3e38ffc6a35863f9d
laghsal riwan <20233810@etud.univ-evry.fr>
2025-11-24 12:56:05 +0100

    correction single quote pour expand var

--- src/parsing/expand_var.py
+++ src/parsing/expand_var.py
@@ -3,12 +3,11 @@ import os
 def expand_var(tokens):
     new_tokens = []
     for token in tokens:
-        if token.startswith("'") and token.endwith("'"):
-            new_tokens.append(token)
-        elif token.startswith("$"):
+        if token.startswith("$"):
             var_name = token[1:]
             value = os.environ.get(var_name, "")
             new_tokens.append(value) # pas de gestion d'erreur comme un vrai shell (ex: echo n'affiche rien)
         else:
             new_tokens.append(token)
     return new_tokens
+
--- src/parsing/tokenizer.py
+++ src/parsing/tokenizer.py
@@ -3,12 +3,22 @@ import os
 import re
 from src.parsing.expand_var import expand_var
 
+
+ALERT_TAG = "__NO_EXPAND__"
+def put_alert(line):
+    return re.sub(r"'([^']*\$[^']*)'", lambda m: ALERT_TAG + m.group(1) + ALERT_TAG, line)
+
+def restore_alert(tokens):
+    return [t.replace(ALERT_TAG, "") for t in tokens]
+
+
 def tokenizer(line):
     line = line.replace('|', ' | ')
     line = re.sub(r'(>>|>|<)', r' \1 ', line)
+    line = put_alert(line)
     tokens = shlex.split(line)
-    tokens = expand_var(tokens)
-    return tokens
+    tokens = restore_alert(tokens)
+    return expand_var(tokens)
 
-# line = "echo bjr>>test.txt>fichier|ls"
-# print(tokenizer(line))
+line = "echo bjr>>test.txt>fichier|ls '$name'"
+print(tokenizer(line))
