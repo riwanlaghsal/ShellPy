ab432003e906e19b376f209925e11f89f2be697f
laghsal riwan <20233810@etud.univ-evry.fr>
2025-11-24 12:59:37 +0100

    test single quote

--- src/parsing/tokenizer.py
+++ src/parsing/tokenizer.py
@@ -9,7 +9,7 @@ def put_alert(line):
     return re.sub(r"'([^']*\$[^']*)'", lambda m: ALERT_TAG + m.group(1) + ALERT_TAG, line)
 
 def restore_alert(tokens):
-    return [t.replace(ALERT_TAG, "") for t in tokens]
+    return [t.replace(ALERT_TAG, "$") for t in tokens]
 
 
 def tokenizer(line):
@@ -17,6 +17,7 @@ def tokenizer(line):
     line = re.sub(r'(>>|>|<)', r' \1 ', line)
     line = put_alert(line)
     tokens = shlex.split(line)
+    print(tokens)
     tokens = restore_alert(tokens)
     return expand_var(tokens)
 
