bcfb9f6729da9c3585739bb730e9a018f6dac6b6
laghsal riwan <20233810@etud.univ-evry.fr>
2025-11-24 12:33:05 +0100

    test single quote expand var

--- src/parsing/expand_var.py
+++ src/parsing/expand_var.py
@@ -3,7 +3,9 @@ import os
 def expand_var(tokens):
     new_tokens = []
     for token in tokens:
-        if token.startswith("$"):
+        if token.startswith("'") and token.endwith("'"):
+            new_tokens.append(token)
+        elif token.startswith("$"):
             var_name = token[1:]
             value = os.environ.get(var_name, "")
             new_tokens.append(value) # pas de gestion d'erreur comme un vrai shell (ex: echo n'affiche rien)
