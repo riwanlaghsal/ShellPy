3c3df12c443b4d401ffc8437e589dc241b048e1a
laghsal riwan <20233810@etud.univ-evry.fr>
2025-12-03 12:41:00 +0100

    add import cd in handle_builtin

--- src/utils/handle_builtins.py
+++ src/utils/handle_builtins.py
@@ -1,5 +1,5 @@
 import sys
-from src.cmd_built_in import cd
+from src.cmd_built_in.cd import cd
 from src.utils.shell_state import shell_state
 
 def handle_builtin(cmd):
@@ -12,12 +12,13 @@ def handle_builtin(cmd):
 
     if cmd["cmd"] == "cd":
         arg = cmd["args"][0] if cmd["args"] else None
-        cd(arg)
+        code = cd(arg)
+        shell_state["?"] = code
         return True
 
     if cmd["cmd"] == "exit":
         code = int(cmd["args"][0]) if cmd["args"] else 0
-        # shell_state["?"] = code
+        shell_state["?"] = code
         sys.exit(code)
 
     return False
