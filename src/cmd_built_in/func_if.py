c7dde519c7c6a814cd4dc19118fbb812d1e4097f
ilyesMeh <20234622@etud-univ.evry.fr>
2025-12-06 00:39:07 +0100

    ajout du builtin_if dans func_if

--- src/cmd_built_in/func_if.py
+++ src/cmd_built_in/func_if.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from src.parsing.parser import parse
 
 
 def condition_check(condition_type, condition1, condition2=None):
@@ -52,9 +53,57 @@ def builtin_if(cmd_struct):
         index_then = args.index("then")
         index_fi = args.index("fi")
     except ValueError:
-        print("Erreur syntaxe: 'then' ou 'fi' manquant",file=sys.stderr)
+        print("Erreur syntaxe: 'then' ou 'fi' manquant", file=sys.stderr)
         return False
 
     index_else = None
     if "else" in args:
         index_else = args.index("else")
+
+    cond = [x for x in args[:index_then] if x!=";"]
+    if not cond:
+        print("Erreur: aucune condition fournie", file=sys.stderr)
+        return False
+
+    is_true = False
+    if len(cond) == 3:
+        is_true = condition_check(cond[1], cond[0], cond[2])
+    elif len(cond) == 2 and cond[0].startswith("-"):
+        is_true = condition_check(cond[0], cond[1])
+    else:
+        if cond[0] == True: is_true = True
+        elif cond [0] == False: is_true = False
+        else:
+            print(f"Erreur: condition mal formée : {cond}", file=sys.stderr)
+            return False
+
+    tokens_to_exec = []
+
+    if is_true:
+        limit = index_then if index_then else index_else
+        tokens_to_exec = args[index_then + 1 : limit]
+    else:
+        if index_else:
+            tokens_to_exec = args[index_else + 1 : index_fi]
+
+    if tokens_to_exec and tokens_to_exec[0] == ";": tokens_to_exec.pop(0)
+    if tokens_to_exec and tokens_to_exec[-1] == ";": tokens_to_exec.pop()
+
+    if not tokens_to_exec:
+        return True
+
+    from src.execution.executor_simple import exec_simple
+    from src.execution.exec_pipeline import exec_pipe
+    from src.utils.handle_builtins import handle_builtin
+
+    parse_if = parse(tokens_to_exec)
+
+    if handle_builtin(parse_if):
+        return True
+
+    if parse_if["type"] == "commande":
+        exec_simple(parse_if)
+    else:
+        exec_pipe(parse_if)
+
+    return True
