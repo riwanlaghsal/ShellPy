4118746230b33e4cd7e732830f840619a40813d7
ilyesMeh <20234622@etud-univ.evry.fr>
2025-12-16 02:56:13 +0100

    ajout des premieres gestions des erreurs

--- src/cmd_built_in/for.py
+++ src/cmd_built_in/for.py
@@ -1,4 +1,17 @@
-def func_for(cmd_args):
-    args = cmd_args["args"]
+import sys
+from src.parsing.parser import parse
+
+def builtin_for(cmd_struct):
+    args = cmd_struct["args"]
+    var_name = args[0]
+
+    if len(args) < 4:
+        print("Erreur syntaxe: for <var> in ... do ... done", file=sys.stderr)
+        return False
+
+    if args[1] != "in":
+        print(f"Erreur syntaxe: attendu 'in',, reçu '{args[1]}'", file=sys.stderr)
+        return False
+
 
 
